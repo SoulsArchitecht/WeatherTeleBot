56aeb5231ca8046df39eb198141fdd65c0924e49
Serge <sshibkojavadev@gmail.com>
2024-10-14 18:02:16 +0800

    init commit

---
+++ src/main/java/ru/sshibko/WeatherTeleBot/WeatherTeleBotApplication.java
@@ -0,0 +1,13 @@
+package ru.sshibko.WeatherTeleBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTeleBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherTeleBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sshibko/WeatherTeleBot/WeatherTeleBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sshibko.WeatherTeleBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherTeleBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
